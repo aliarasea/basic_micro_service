c3bd3bb87aa6983d7b31feb2dee184ad396e7a13
aliarasea <aliaras@outlook.com>
2023-11-29 13:04:01 +0300

    discovery node

---
+++ node_discovery/src/test/java/com/aliarasea/j/basic_micro_service/node_discovery/NodeDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliarasea.j.basic_micro_service.node_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodeDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
